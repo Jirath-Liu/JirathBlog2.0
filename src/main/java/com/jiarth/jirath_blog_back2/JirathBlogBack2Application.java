266c15b58dc9acbf7a5ff5514260617555d3fe6d
刘元盛 <jirath@qq.com>
2019-12-13 14:16:03 +0800

    fix configuration

---
+++ src/main/java/com/jiarth/jirath_blog_back2/JirathBlogBack2Application.java
@@ -0,0 +1,13 @@
+package com.jiarth.jirath_blog_back2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JirathBlogBack2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JirathBlogBack2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/jiarth/jirath_blog_back2/JirathBlogBack2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiarth.jirath_blog_back2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JirathBlogBack2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
